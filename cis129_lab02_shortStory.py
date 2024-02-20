1773e5b7c417799f27c94c474aa529de9feb644a
Galindo-Martinez <160555939+Galindo-Martinez@users.noreply.github.com>
2024-02-20 11:37:21 -0700

    Update cis129_lab02_shortStory.py
    This edit contains a short story about an agile character.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Ana Galindo
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print("Hello, I am Mac. I live in a huge house.")
+print("I love chasing my sister around and doing parkour.")
+print("I bounce off the couch, the walls and even my mouse.")
+print("Moca is to lazy to bounce, but that's what I adore.")
+print("Can you guess what I am?")
+print("I'm a cat of course!")
